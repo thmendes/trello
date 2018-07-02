18b4c216608a45798fb3c43dc95f24b4309ab228
Thiago Mendes <th.thiago.mendes@gmail.com>
2018-07-01 23:30:29 -0300

    Add vue

---
+++ public/src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <div class="row col-md-12">
+            <list-boards></list-boards>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ListBoards from './ListBoards.vue';
+    export default {
+        name: 'app',
+        data(){
+            return {}
+        },
+        components: { ListBoards },
+    }
+</script>
+
+<style>
+
+</style>
+
---
+++ public/src/components/ListBoards.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Boards
+    </div>
+</template>
