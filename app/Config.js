3c3afa8be8891c7c63eb3a6e189ad31b43e3eb12
Thiago Mendes <th.thiago.mendes@gmail.com>
2018-07-02 23:08:56 -0300

    .

--- app/Config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    DB: 'mongodb://localhost:27017/todos',
-    APP_PORT: 4000,
-    KEY: 'c1a5f25577637ea9d5d700071109e75b',
-    TOKEN: '66353654965b8d4cd341adab54bb9228df5c0c65bc0dfdba65608c0871930a06'
-  }
