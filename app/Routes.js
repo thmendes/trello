9b4ad7febf61b91745cbfd6ca0196f7f623324e7
Thiago Mendes <th.thiago.mendes@gmail.com>
2018-07-02 23:00:43 -0300

    added ListBoards

--- app/Routes.js
+++ app/Routes.js
@@ -4,10 +4,26 @@ var express = require('express')
 
 var trelloSpyRoutes = express.Router()
 
-var trello = require('./trello')
+var Trello = require('./trello')
+
+var config = require('./Config')
+
+var trello = new Trello(config.KEY, config.TOKEN)
 
 trelloSpyRoutes.route('/ping').get(function (req, res, next){
     res.json('pong')
+})
+
+trelloSpyRoutes.route('/boards').get(function (req, res, next){
+    trello.getBoards('me', function(error, boards){
+        if(error){
+            console.log('Could not get boards')
+        }
+        else{
+            console.log('Boards rescued')
+            res.json(JSON.parse(JSON.stringify(boards)))
+        }
+    })
 })
 
 module.exports = trelloSpyRoutes
--- public/src/components/ListBoards.vue
+++ public/src/components/ListBoards.vue
@@ -1,5 +1,61 @@
 <template>
     <div>
-        Boards
+        <h3>Boards</h3>
+        <div class="col-md-12" v-show="boards.length > 0">
+            <div class="row mrb-10" v-for="board in boards" :key="board.id">
+                <div class="input-group m-b-5">
+                    {{ board.name }}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+    import axios from 'axios'
+    import bus from './../bus.js'
+
+    export default {
+        data() {
+            return {
+                boards: []
+            }
+        },
+        created: function(){
+            this.fetchBoard();
+        },
+        methods: {
+            fetchBoard(){
+                let uri = 'http://localhost:4000/api/boards';
+                axios.get(uri).then((response) => {
+                    this.boards = response.data;
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .todo__done {
+        text-decoration: line-through !important
+    }
+    .no_border_left_right {
+        border-left: 0px;
+        border-right: 0px;
+    }
+    .flat_form {
+        border-radius: 0px;
+    }
+    .mrb-10 {
+        margin-bottom: 10px;
+    }
+    .addon-left {
+        background-color: none !important;
+        border-left: 0px !important;
+        cursor: pointer !important;
+    }
+    .addon-right {
+        background-color: none !important;
+        border-right: 0px !important;
+    }
+</style>
